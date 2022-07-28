f023b008f802ed4983d6c6fb5672ec3da451fb72
Antonio Carrizosa <antonio@bitfx.mx>
2022-07-27 22:39:40 -0500

    sort functionality added

--- js/app.js
+++ js/app.js
@@ -12,6 +12,8 @@ class Character {
 
     // los getters no se invocan, se comportan como propiedades
     get fullName() {
+        // case firstname is empty [Pycelle]
+        if (!this.firstName.length) return this.lastName;
         return `${this.firstName} ${this.lastName}`;
     }
 }
@@ -20,10 +22,12 @@ const mainEL = document.querySelector('main');
 const searchInput = document.querySelector('#search');
 const clearEl = document.querySelector('#clear');
 const familiesSelect = document.querySelector('#families');
+const sortEl = document.querySelector('#sort');
 
 let characters = [];
 let filteredData = [];
 let families = [];
+let ascSort = true;
 
 
 // predicados son funciones que retornan un valor booleano
@@ -42,6 +46,7 @@ const getData = async () => {
     characters = data.map(character => new Character({ ...character }));
     filteredData = characters;
     generateSelect(characters);
+    filteredData.sort((a, b) => a.fullName.localeCompare(b.firstName));
     renderData(filteredData);
 }
 
@@ -89,13 +94,11 @@ const fillSelect = (data = []) => {
 const reset = () => {
     searchInput.value = '';
     filteredData = characters;
-    const family = familiesSelect.value;
-    // se construye como si fuera un evento de input
-    filterDataByFamily({ target: { value: family } });
+    filterData();
 }
 
-const filterData = (e) => {
-    const query = e.target.value;
+const filterData = () => {
+    const query = searchInput.value;
     const family = familiesSelect.value;
     if (family == 'All') {
         // se pasan a lowecase para filtrar independientemente de si se escribe en mayusculas o minusculas
@@ -104,20 +107,20 @@ const filterData = (e) => {
         filteredData = characters.filter(character =>
             isSearchResultAndIsSameFamily(query, character, family));
     }
+
+    if (ascSort) {
+        filteredData.sort((a, b) => a.fullName.localeCompare(b.firstName));
+    } else {
+        filteredData.sort((a, b) => b.fullName.localeCompare(a.firstName));
+    }
     renderData(filteredData);
 }
 
-const filterDataByFamily = (e) => {
-    const query = searchInput.value;
-    const family = e.target.value;
-    if (family == 'All') {
-        filteredData = characters.filter(character =>
-            isSearchResult(query, character));
-    } else {
-        filteredData = characters.filter(character =>
-            isSearchResultAndIsSameFamily(query, character, family));
-    }
-    renderData(filteredData);
+const onSortChanged = () => {
+    const arrow = ascSort ? '&#8595' : '&#8593';
+    sortEl.children[0].innerHTML = `<span>${arrow}</span>`;
+    ascSort = !ascSort;
+    filterData();
 }
 
 searchInput.addEventListener('input', filterData);
@@ -125,6 +128,8 @@ searchInput.addEventListener('input', filterData);
 // las firmas no coinciden pero esto solo funciona en javascript
 clearEl.addEventListener('click', reset);
 
-familiesSelect.addEventListener('change', filterDataByFamily);
+familiesSelect.addEventListener('change', filterData);
+
+sortEl.addEventListener('click', onSortChanged);
 
 getData();
